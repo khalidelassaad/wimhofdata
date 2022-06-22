88ecd9c07ed5e1a491cef973f639189ce32885dc
Khalid Elassaad <khalidelassaad@gmail.com>
2022-06-21 17:42:34 -0700

    code: convert seconds to M:SS and format string nicely

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+let timeInSeconds = 0;
+
+function secondsToTimeString(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  let secondsString =
+    remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+  return `${minutes}:${secondsString}`;
+}
+
 function handleTimerStart() {
   console.log("START THE TIMER!!!");
+  $("#timer-container").text(secondsToTimeString(timeInSeconds));
+  timeInSeconds += 1;
 }
 
 $("#timer-start-button").click(function (e) {
